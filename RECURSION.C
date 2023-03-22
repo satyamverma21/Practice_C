7d66065de2ebab91bf87140e72dfd68ea2549e8e
SATYAM VERMA <satyamvermabca2021@delhitechnicalcampus.ac.in>
2023-03-22 07:20:27 +0530

    .

--- RECURSION.C
+++ RECURSION.C
@@ -1,19 +1,23 @@
+//sum of n natural nums using recurtion
+
 #include<stdio.h>
-int factorial(int n);
+#include<stdlib.h>
+
+int sum(int n );
+
 int main()
 {
- int n ;
- printf("enter the num u want to find factorial of ");
- scanf("%d",&n);
- printf("factorial of %d is %d ",n ,factorial(n));
-
+    int n ,a;
+    printf("enter number : ");
+    scanf("%d",&n);
+    a=sum(n);
+    printf("sum is %d",a);
+    return 0;
 }
 
-int factorial(int n)
-{
-//   printf("%d\n",n); //this help to check how code is repeating itself with diff values. 
-  
-     if (n==1 || n==0) {return 1 ;}
-     else {return n * factorial(n-1);}
-
+int sum(int n)
+{ 
+  if(n==0){return 0;}
+    return n+sum(n-1);
 }
+
